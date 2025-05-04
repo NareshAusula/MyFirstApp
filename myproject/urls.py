1cc881374d5c2d1489027a6a94ec02ddd45d9844
NareshAusula <Naniraina108@gmail.com>
2025-05-04 08:01:34 +0530

    changes made in myproject/urls.py to redirect Onlinecourse/urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib import admin  # type: ignore
+from django.urls import include, path  # type: ignore
+from django.conf.urls.static import static  # type: ignore
+from django.conf import settings  # type: ignore
+from django.views.generic import RedirectView  # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path(' ', RedirectView.as_view(url='onlinecourse/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
